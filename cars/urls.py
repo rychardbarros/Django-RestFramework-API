24577fb735f23be11ed9cae18e62b86c9597f525
rychardbarros <rychard.barros.dev@gmail.com>
2023-08-03 16:54:30 -0300

    jwt

--- cars/urls.py
+++ cars/urls.py
@@ -2,11 +2,18 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from core.api import viewsets as carsViewSets
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 route = routers.DefaultRouter()
 route.register(r'cars', carsViewSets.CarsViewSets, basename='Cars')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include(route.urls))
+    path('', include(route.urls)),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
 ]
--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -1,8 +1,10 @@
 from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
 from core.api.serializers import CarsSerializer
 from core import models
 
 class CarsViewSets(viewsets.ModelViewSet):
+    permission_classes = (IsAuthenticated, )
+
     serializer_class = CarsSerializer
     queryset = models.Cars.objects.all()
-
